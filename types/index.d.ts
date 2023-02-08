899c39c64a7c2ac4786c98749fc34e521f1f6d6b
svonme <svon.me@gmail.com>
2023-02-08 09:53:48 +0800

    doc: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,5 +7,6 @@ export * as table from "./utils/table";
 export * as path from "./utils/path";
 export * as rule from "./utils/rule";
 export * as useState from "./utils/http";
+export * from "./utils/api";
 export * from "./utils/string/format";
 export * from "./utils/uuid";
