92eaa2713467b30b8b88490493a792612b1c4adb
svonme <svon.me@gmail.com>
2023-08-18 16:04:20 +0800

    feat: pagination

--- src/http/index.ts
+++ src/http/index.ts
@@ -5,14 +5,16 @@
  */
 
 import * as _ from "lodash-es";
-import { toRaw } from "vue";
+import { computed, ref, toRaw } from "vue";
 import { PageResult } from "../api";
 import { useAsyncState } from "@vueuse/core";
 import type { UseAsyncStateOptions, UseAsyncStateReturn } from "@vueuse/core";
+import { TablePaginationConfig } from "ant-design-vue";
 
-interface UseStateResult<Data, Params extends any[], Shallow extends boolean = true> extends UseAsyncStateReturn<Data, Params, Shallow>{
+interface UseStateResult<Data, Params extends any[]> extends UseAsyncStateReturn<Data, Params, true>{
   setState: (value: Data | object) => void
 }
+
 
 const listNormalize = function<T>(list: T[], tableId: string = "id") {
   return _.map(list, function(item: T) {
@@ -28,12 +30,13 @@ const listNormalize = function<T>(list: T[], tableId: string = "id") {
   });
 };
 
-export const data = function<T = any, Shallow extends boolean = true>(
-  api: Promise<T> | ((...args: any[]) => Promise<T>),
+export const data = function<T = any>(
+  api: Promise<T> | ((...args: any[]) => T) | ((...args: any[]) => Promise<T>),
   initialState?: T, 
-  options?: UseAsyncStateOptions<Shallow>
-): UseStateResult<T, any, Shallow> {
-  const value = useAsyncState<T>(api, (initialState || {}) as any, options as any);
+  options?: UseAsyncStateOptions<true>
+): UseStateResult<T, any> {
+  // @ts-ignore
+  const value = useAsyncState<T>(api, (initialState || {}), options);
   const setState = function(state: T | object) {
     const data = toRaw(value.state.value);
     value.state.value = { ...data, ...state };
@@ -41,27 +44,24 @@ export const data = function<T = any, Shallow extends boolean = true>(
   return { ...value, setState } as any;
 };
 
-export const dataExecute = function<T = any, Shallow extends boolean = true>(
-  api: (...args: any[]) => Promise<T>, 
+export const dataExecute = function<T = any>(
+  api: ((...args: any[]) => T) | ((...args: any[]) => Promise<T>),
   initialState?: T, 
-  options: UseAsyncStateOptions<Shallow> = {
+  options: UseAsyncStateOptions<true> = {
     immediate: false,
     resetOnExecute: false
   }
-): UseStateResult<T, any, Shallow> {
-  return data<T, Shallow>(api, initialState, options);
+): UseStateResult<T, any> {
+  return data<T>(api, initialState, options);
 }
 
-export const list = function<T = any, Shallow extends boolean = true>(
-  api: Promise<PageResult<T>> | ((...args: any[]) => Promise<PageResult<T>>) | PageResult<T>, 
+export const list = function<T = any>(
+  api: Promise<PageResult<T>> | ((...args: any[]) => PageResult<T>) | ((...args: any[]) => Promise<PageResult<T>>) | PageResult<T>, 
   initialState?: PageResult<T>, 
-  options?: UseAsyncStateOptions<Shallow>,
+  options?: UseAsyncStateOptions<true>,
   tableId?: string
-): UseAsyncStateReturn<PageResult<T>, any, Shallow> {
-  const value = initialState || {
-    total: 0,
-    results: []
-  };
+): UseAsyncStateReturn<PageResult<T>, any, true> {
+  const value = initialState || new PageResult<T>();
   const ajax = async function(...args: any[]) {
     let data: PageResult<T>;
     let temp: PageResult<T> | T[];
@@ -84,13 +84,67 @@ export const list = function<T = any, Shallow extends boolean = true>(
   return useAsyncState<PageResult<T>>(ajax, value, options);
 };
 
-export const listExecute = function<T = any, Shallow extends boolean = true>(
-  api: ((...args: any[]) => Promise<PageResult<T>>), 
+export const listExecute = function<T = any>(
+  api: ((...args: any[]) => PageResult<T>) | ((...args: any[]) => Promise<PageResult<T>>), 
   initialState?: PageResult<T>, 
-  options: UseAsyncStateOptions<Shallow> = {
+  options: UseAsyncStateOptions<true> = {
     immediate: false,
     resetOnExecute: false
   }
-): UseAsyncStateReturn<PageResult<T>, any, Shallow> {
+): UseAsyncStateReturn<PageResult<T>, any, true> {
   return list(api, initialState, options);
+}
+
+export const paginationExecute = function<T = any>(
+  api: ((...args: any[]) => PageResult<T>) | ((...args: any[]) => Promise<PageResult<T>>), 
+  limit: number = 10,
+) {
+  let current = ref<number>(1);
+  let pageSize = ref<number>(limit);
+
+  const get = function(...args: any[]): Promise<PageResult<T>> {
+    if (typeof api === "function") {
+      const param = { current: current.value, pageSize: pageSize.value };
+      return Promise.resolve(api(param, ...args));
+    }
+    return Promise.resolve(api);
+  };
+  const { state, execute, ...useValue } = listExecute<T>(get);
+
+  const total = computed<number>({
+    get: () => {
+      // @ts-ignore
+      return state.value.total || 0;
+    },
+    set: (value: number) => {
+      // @ts-ignore
+      state.value.total = value;
+    }
+  });
+
+  const pagination = computed<TablePaginationConfig>(function() {
+    return {
+      current: current.value,
+      total: total.value,
+      pageSize: pageSize.value,
+      size: "default",
+      onChange: function(page: number, limint: number, ...args) {
+        console.log(args);
+        pageSize.value = limint;
+        current.value = page;
+        execute(50);
+      }
+    };
+  });
+
+  return { state, execute, ...useValue, current, pageSize, total, pagination };
+}
+
+export const pagination = function<T = any>(
+  api: ((...args: any[]) => PageResult<T>) | ((...args: any[]) => Promise<PageResult<T>>), 
+  limit: number = 10,
+) {
+  const value = paginationExecute(api, limit);
+  value.execute(0);
+  return value;
 }
